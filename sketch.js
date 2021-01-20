f5db45d47c6f41b8065f612c2610a8895eb40c43
KennithBrian <75786380+KennithBrian@users.noreply.github.com>
2021-01-20 09:52:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var box1,box1,box3;
+var paper;
+var ground
+function setup() 
+{
+	createCanvas(1200,400);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(601,350,1202,10);
+	box1 = new Box(1000,340,200,10);
+	box2 = new Box(900,295,10,100);
+	box3 = new Box(1100,295,10,100);
+
+	paper = new Paper();
+
+	Engine.run(engine);  
+}
+
+
+function draw()
+{
+  rectMode(CENTER);
+  background(0);
+
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  paper.display();
+}
+
+function keyPressed(){
+  if (keyCode === UP_ARROW) {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 30,
+      y: -31
+    });
+  }
+}
